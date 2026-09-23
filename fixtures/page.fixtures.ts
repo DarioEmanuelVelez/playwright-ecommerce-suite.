1280230bc45abced3bf9bf2d8cd81906915f70a8
Dario Velez <dario.velez@VEMOTO.COM>
2026-09-23 13:51:16 -0300

    feat(fixtures): add custom page fixtures for login and inventory pages

---
+++ fixtures/page.fixtures.ts
@@ -0,0 +1,22 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { InventoryPage } from '../pages/inventory.page';
+
+type MyFixtures = {
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+};
+
+export const test = base.extend<MyFixtures>({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await use(loginPage);
+  },
+
+  inventoryPage: async ({ page }, use) => {
+    const inventoryPage = new InventoryPage(page);
+    await use(inventoryPage);
+  },
+});
+
+export { expect } from '@playwright/test';
